4e8501690be50c17289204ca60f751752068440f
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-08-01 19:18:35 -0300

    feat: componente features em desenvolvimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ManagerComponent } from './manager/manager.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ManagerComponent
+    ManagerComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule
