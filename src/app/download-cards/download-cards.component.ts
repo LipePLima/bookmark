e8db3f1d5277dfa6bc6eb41b0e5aa4f85391e639
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-08-02 20:17:50 -0300

    feat: componente download-cards devidamente dinâmico

--- src/app/download-cards/download-cards.component.ts
+++ src/app/download-cards/download-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-download-cards',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./download-cards.component.scss']
 })
 export class DownloadCardsComponent {
-
+  @Input() cardInfo: {
+    imageSrc: string;
+    cardTitle: string;
+    cardText: string;
+    buttonText: string;
+  } | undefined;
 }
