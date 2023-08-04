208575280d92b755cab28439722b0b501294981c
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-08-04 14:20:19 -0300

    feat: interação com menu hamburguer desenvolvida

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  public logo: string    = "../../assets/images/logo-bookmark.svg";
+  public menu: string    = "../../assets/images/icon-hamburger.svg"
+  public isOpen: boolean = false;
 
+  public openMenu() {
+    this.isOpen = !this.isOpen;
+
+    this.menu = (this.menu === "../../assets/images/icon-hamburger.svg")
+      ? "../../assets/images/icon-close.svg"
+      : "../../assets/images/icon-hamburger.svg";
+
+    this.logo = (this.logo === "../../assets/images/logo-bookmark.svg")
+      ? "../../assets/images/logo-bookmark-menu.svg"
+      : "../../assets/images/logo-bookmark.svg";
+  }
 }
