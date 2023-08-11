533bdc54eacc02d06c3359c2455ee110f5b23a6a
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-08-11 16:12:30 -0300

    style: media queries para tablet feito

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,22 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './headerMQ.component.scss']
 })
+
 export class HeaderComponent {
   public logo: string    = "../../assets/images/logo-bookmark.svg";
-  public menu: string    = "../../assets/images/icon-hamburger.svg"
+  public menu: string    = "../../assets/images/icon-hamburger.svg";
   public isOpen: boolean = false;
+
+  constructor() {
+    this.updateIsOpenBasedOnScreenWidth();
+    window.addEventListener('resize', this.updateIsOpenBasedOnScreenWidth.bind(this));
+  }
+
+  private updateIsOpenBasedOnScreenWidth() {
+    const newIsOpen = window.innerWidth >= 768;
+
+    if (this.isOpen !== newIsOpen) {
+      this.isOpen = newIsOpen;
+    }
+  }
 
   public openMenu() {
     this.isOpen = !this.isOpen;
+    this.updateLogoAndMenu();
+  }
 
-    this.menu = (this.menu === "../../assets/images/icon-hamburger.svg")
+  private updateLogoAndMenu() {
+    this.menu = this.isOpen
       ? "../../assets/images/icon-close.svg"
       : "../../assets/images/icon-hamburger.svg";
 
-    this.logo = (this.logo === "../../assets/images/logo-bookmark.svg")
+    this.logo = this.isOpen
       ? "../../assets/images/logo-bookmark-menu.svg"
       : "../../assets/images/logo-bookmark.svg";
   }
 }
+
